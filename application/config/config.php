4b2357c000fa426a2fd04bb5522198e89d8942b4
Anbuselvan Rocky <anbuceo@gmail.com>
2019-05-15 21:59:55 +0530

    Improvements

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/--projects/git/Stripe-Payment-Gateway-Integration-CodeIgniter';
+$config['base_url'] = '#';
 
 /*
 |--------------------------------------------------------------------------
